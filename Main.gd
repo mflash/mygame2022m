470336087a0c64173aea277d1df2d7ddf8a6ee9d
Marcelo Cohen <mflashbr@gmail.com>
2022-09-13 08:38:49 -0300

    Tratamento de eventos com callback e polling

--- Main.gd
+++ Main.gd
@@ -1,13 +1,22 @@
 extends Node2D
 
+const SPEED : int = 100
 var total : float = 0
 
 func _ready() -> void:
 	update_score(total)
 	
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("right"):
+		print("Right arrow")
+	
 func _process(delta: float) -> void:
 	#inc_score(delta)
 	update_score(total)
+	
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("right"):
+		position.x += SPEED * delta
 	
 func inc_score(incr: float) -> void:
 	total += incr
